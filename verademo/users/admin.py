fe3abbaa2fde8d652a9c8d8f934ff4268e4d48aa
clydeshtino <clydeshtino@gmail.com>
2024-05-31 15:24:33 -0400

    Anything

--- verademo/users/admin.py
+++ verademo/users/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Blab, Blabber, Comment
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'password']
+
+
+@admin.register(Blab)
+class BlabAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'postDate', 'commentCount']
+
--- verademo/users/models.py
+++ verademo/users/models.py
@@ -5,3 +5,74 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     
+class Blab(models.Model):
+
+    content = models.TextField(max_length=1000)
+    postDate = models.DateTimeField(auto_now_add=True)
+    commentCount = models.IntegerField(default=0)
+    author = models.TextField(max_length=100)
+    
+    def __str__(self):
+        return self.id
+    
+    def __str2__(self):
+        return self.content
+    
+    def __str3__(self):
+        return self.postDate
+    
+    def __str4__(self):
+        return self.commentCount
+    
+    def __str5__(self):
+        return self.author
+     
+class Blabber(models.Model):
+    username = models.TextField(max_length=100)
+    realName = models.TextField(max_length=100)
+    blabName = models.TextField(max_length=100)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    numberListeners = models.IntegerField(default=0)
+    numberListening = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.id
+    
+    def __str2__(self):
+        return self.username
+    
+    def __str3__(self):
+        return self.realName
+    
+    def __str4__(self):
+        return self.blabName
+    
+    def __str5__(self):
+        return self.createdDate
+    
+    def __str6__(self):
+        return self.numberListeners
+    
+    def __str7__(self):
+        return self.numberListening
+    
+class Comment(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField
+    author = models.TextField()
+
+    def __str__(self):
+        return self.id
+    
+    def __str2__(self):
+        return self.content
+    
+    def __str3__(self):
+        return self.timestamp
+    
+    def __str4__(self):
+        return self.author
+
+
+
+
