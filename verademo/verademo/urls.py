9063363366fde7b0a7b1f2c6519223def70eca4d
Stuart Sessions <ssessions@ssessions-mac.veracode.local>
2024-05-31 14:48:46 -0400

    created interactions app

--- verademo/verademo/urls.py
+++ verademo/verademo/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('users/', include('users.urls'))
 ]
