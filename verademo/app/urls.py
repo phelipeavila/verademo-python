e6594da72576b8ef33f6f838c7a737cfe1cd7e76
kevliu43 <kevliu43@gmail.com>
2024-06-03 10:29:41 -0400

    switched to method-based

--- verademo/app/urls.py
+++ verademo/app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.LoginView.as_view()),
-    path('login', views.LoginView.as_view()),
-    path('register/', views.register),
+    path('', views.login),
+    path('login', views.login),
+    path('register', views.register),
     path('hello/',views.say_hello),
 ]
