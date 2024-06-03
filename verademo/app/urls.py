734d829abd341b9ec23e66f74351b3f13856dbf6
Stuart Sessions <ssessions@veracode.com>
2024-06-03 12:52:36 -0400

    Registry form working

--- verademo/app/urls.py
+++ verademo/app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 from . import views
 
 urlpatterns = [
     path('', views.login),
     path('login', views.login),
     path('register', views.register),
+    path('reg-return', views.user_create_view),
     path('hello/',views.say_hello),
 ]
--- verademo/app/views.py
+++ verademo/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from django.views.generic import TemplateView
+
+from .forms import UserForm
 
 # Deals with HTTP request/response
 def say_hello(request):
@@ -12,6 +13,17 @@ def register(request):
 
 def login(request):
     return render(request, 'app/login.html',{})
+
+def user_create_view(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        print('help?')
+    context = {
+        'form': form
+    }
+    print("help")
+    return render (request, 'app/login.html')
 
 class LoginView(TemplateView):
     template_name = 'app/login.html'
