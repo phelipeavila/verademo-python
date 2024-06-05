187a7bc6b4156a7f91a0964153eb3d4fe5733f2d
clydeshtino <clydeshtino@gmail.com>
2024-06-05 11:52:01 -0400

    Tools page fixed, reset page completed

--- verademo/app/views.py
+++ verademo/app/views.py
@@ -144,10 +144,137 @@ def processRegisterFinish(request):
         '''
     return render (request, 'app/feed.html')
 
-        
-    return render (request, 'app/feed.html')
+def home(request):
+    # Equivalent of HomeController.java
+    if request.session.get('username'):
+        return redirect('feed')
+    
+    return login(request)
 
+def feed(request):
+    return render(request, 'app/feed.html', {})
 
+def login(request):
+    if request.method == "GET":
+
+        target = request.GET.get('target')
+        username = request.GET.get('username')
+
+        if request.session.get('username'):
+            logger.info("User is already logged in - redirecting...")
+            if (target != None) and (target) and (not target == "null"):
+                return redirect(target)
+            else:
+                return redirect('feed')
+
+        userDetailsCookie = request.COOKIES.get('user')
+        if userDetailsCookie is None or not userDetailsCookie:
+            logger.info("No user cookie")
+            userDetailsCookie = None
+            if username is None:
+                username = ''
+            if target is None:
+                target = ''
+            logger.info("Entering login with username " + username + " and target " + target)
+            
+            request.username = username
+            request.target = target
+
+        else:
+            logger.info("User details were remembered")
+            unencodedUserDetails = next(serializers.deserialize('xml', userDetailsCookie))
+
+            logger.info("User details were retrieved for user: " + unencodedUserDetails.object.username)
+            
+            request.session['username'] = unencodedUserDetails.object.username
+
+            if (target != None) and (target) and (not target == "null"):
+                return redirect(target)
+            else:
+                return redirect('feed')
+
+        return render(request, 'app/login.html', { "username": username, "target": target })
+    
+    if request.method == "POST":
+        logger.info("Processing login")
+
+        username = request.POST.get('user')
+        password = request.POST.get('password')
+        remember = request.POST.get('remember')
+        target = request.POST.get('target')
+
+        if (target != None) and (target) and (not target == "null"):
+            nextView = target
+            response = redirect(nextView)
+        else:
+            nextView = 'feed'
+            response = redirect(nextView)
+
+        try:
+            logger.info("Creating the Database connection")
+            with connection.cursor() as cursor:
+                logger.info("Creating database query")
+
+                # TODO: Replace with md5 hash after register uses MD5
+                # sqlQuery = "select username, password, hint, dateCreated, lastLogin, \
+                #             realName, blabName from app_user where username='" + username + "' \
+                #             and password='" + hashlib.md5(password.encode('utf-8')).hexdigest() + "';"
+                sqlQuery = "select username, password, hint, dateCreated, lastLogin, \
+                            realName, blabName from app_user where username='" + username + "' \
+                            and password='" + password + "';"
+                
+                cursor.execute(sqlQuery)
+                row = cursor.fetchone()
+
+                if (row):
+                    columns = [col[0] for col in cursor.description]
+                    row = dict(zip(columns, row))
+                    logger.info("User found")
+                    response.set_cookie('username', username)
+                    if (not remember is None):
+                        currentUser = User(username=row["username"],
+                                    hint=row["hint"], dateCreated=row["dateCreated"],
+                                    lastLogin=row["lastLogin"], realName=row["realName"], 
+                                    blabName=row["blabName"])
+                        response = update_in_response(currentUser, response)
+                    request.session['username'] = row['username']
+
+                    update = "UPDATE app_user SET lastLogin=date('now') WHERE username='" + row['username'] + "';"
+                    cursor.execute(update)
+                else:
+                    logger.info("User not found")
+
+                    request.error = "Login failed. Please try again."
+                    request.target = target
+
+                    nextView = 'login'
+                    response = render(request, 'app/' + nextView + '.html', {})
+        except:
+
+            # TODO: Implement exceptions
+
+            logger.error("Unexpected error:", sys.exc_info()[0])
+
+            nextView = 'login'
+            response = render(request, 'app/' + nextView + '.html', {})
+
+        logger.info("Redirecting to view: " + nextView)
+            
+        return response
+
+def logout(request):
+    logger.info("Processing logout")
+    request.session['username'] = None
+    response = redirect('login')
+    response.delete_cookie('user')
+    logger.info("Redirecting to login...")
+    return response
+
+def update_in_response(user, response):
+    cookie = serializers.serialize('xml', [user,])
+    response.set_cookie('user', cookie)
+    return response
+    
 def notImplemented(request):
     return render(request, 'app/notImplemented.html')
 
