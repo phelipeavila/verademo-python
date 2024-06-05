73699cbddf56f6093b9e7949ca7f0b070c049f93
kevliu43 <kevliu43@gmail.com>
2024-06-05 10:20:49 -0400

    merged the register functionality

--- verademo/app/views.py
+++ verademo/app/views.py
@@ -9,28 +9,137 @@ from app.models import User
 from django.core import serializers
 import sys
 
-from .forms import UserForm
+from .forms import UserForm, RegisterForm
 
 # Get logger
 logger = logging.getLogger("__name__")
 
-def registerHandler(request):
+def feed(request):
+    return render(request, 'app/feed.html', {})
+
+def blabbers(request):
+    return render(request, 'app/blabbers.html', {})
+
+def profile(request):
+    return render(request, 'app/profile.html', {})
+
+def tools(request):
+    return render(request, 'app/tools.html', {})
+
+def profile(request):
     if(request.method == "GET"):
-        return register(request)
+        return showProfile(request)
     elif(request.method == "POST"):
-        return finish_register(request)
+        return processProfile(request)
+    
+def showProfile(request):
+    pass
+
+def processProfile(request):
+    pass
+
+def register(request):
+    if(request.method == "GET"):
+        return showRegister(request)
+    elif(request.method == "POST"):
+        return processRegister(request)
 
 '''
 renders the register.html file, called by a path in urls
 '''
-def register(request):
+def showRegister(request):
     return render(request, 'app/register.html', {})
 
-def finish_register(request):
-    context = {
-        'username':request.POST.get('username')
-    }
-    return render(request, 'app/register-finish.html', context)
+''' Sends username into register-finish page'''
+def processRegister(request):
+    request.session['username'] = request.POST.get('username')
+    return render(request, 'app/register-finish.html')
+
+def registerFinish(request):
+    if(request.method == "GET"):
+        return showRegisterFinish(request)
+    elif(request.method == "POST"):
+        return processRegisterFinish(request)
+
+def showRegisterFinish():
+    logger.info("Entering showRegisterFinish")
+    pass
+
+'''
+Interprets POST request from register form, adds user to database
+'''
+'''
+TODO:Manually input registrations using SQL statements.
+- may not work because of change to username field
+'''
+def processRegisterFinish(request):
+    logger.info("Entering processRegisterFinish")
+    #create variables
+    username = request.session.get('username')
+    cpassword = request.POST.get('cpassword')
+    #fill in required username field
+    form = RegisterForm(request.POST or None)
+    #user now should have all the required fields
+
+    if form.is_valid():
+        password = form.cleaned_data.get('password')
+        #Check if passwords from form match
+        if password != cpassword:
+            logger.info("Password and Confirm Password do not match")
+            request.error = "The Password and Confirm Password values do not match. Please try again."
+            return render(request, 'app/register.html')
+        sqlStatement = None
+        try:
+            # Get the Database Connection
+            logger.info("Creating the Database connection")
+            with connection.cursor() as cursor:
+                # START EXAMPLE VULNERABILITY 
+                # Execute the query
+
+                #set variables to make easier to use
+                realName = form.cleaned_data.get('realName')
+                blabName = form.cleaned_data.get('blabName')
+                mysqlCurrentDateTime = datetime.now().strftime('YYYY-MM-DD HH:MM:SS')
+                #create query
+                query = ''
+                query += "insert into users (username, password, created_at, real_name, blab_name) values("
+                query += ("'" + username + "',")
+                query += ("'" + realName + "',")
+                # TODO: Implement hashing
+                #query += ("'" + BCrypt.hashpw(password, BCrypt.gensalt()) + "',")
+                query += ("'" + password + "',")
+                query += ("'" + mysqlCurrentDateTime + "',")
+                query += ("'" + blabName + "'")
+                query += (");")
+                #execute query
+                #test
+                cursor.execute(query)
+                sqlStatement = cursor.fetchone() #<- variable for response
+                logger.info(query)
+                # END EXAMPLE VULNERABILITY
+        #TODO: Implement exceptions and final statement
+        except: # SQLException, ClassNotFoundException as e:
+            logger.error()
+        '''
+        finally:
+            try:
+                if sqlStatement != None:
+                    #sqlStatement.close();
+                
+            except SQLException as exceptSql
+                logger.error(exceptSql)
+            try:
+                if (connect != null) {
+                    connect.close();
+                }
+            } catch (SQLException exceptSql) {
+                logger.error(exceptSql);
+            }
+        '''
+        
+
+        
+    return render (request, 'app/feed.html')
 
 def home(request):
     # Equivalent of HomeController.java
