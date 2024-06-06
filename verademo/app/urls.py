af52c05df022e3c3814caf7f777d01b3e1ae9749
Stuart Sessions <ssessions@veracode.com>
2024-06-06 16:07:25 -0400

    added comma to urls

--- verademo/app/urls.py
+++ verademo/app/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('tools', views.tools, name='tools'),
     path('notImplemented', views.notImplemented, name='notImplemented'),
     path('reset', views.reset, name='reset'),
-    path('downloadprofileimage', views.downloadImage, name='downloadProfileImage')
-    path('blab', views.blab, name='blab')
+    path('downloadprofileimage', views.downloadImage, name='downloadProfileImage'),
+    path('blab', views.blab, name='blab'),
     #path('hello/',views.say_hello),
     # path('register/', views.register),
 ]
