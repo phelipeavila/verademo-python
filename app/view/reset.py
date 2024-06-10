8d950d82242d3de5506e671afecd928ffaaabfbd
Stuart Sessions <ssessions@veracode.com>
2024-06-10 16:51:35 -0400

    processReset written

--- app/view/reset.py
+++ app/view/reset.py
@@ -1,18 +1,20 @@
 from django.shortcuts import redirect, render
-from django.db import connection, transaction
+from django.db import connection, transaction, IntegrityError
 
 from app.models import User
 from app.views import create
 
-import logging, random
-# START VULNERABLE PACKAGE
+import sqlite3
+import logging
+import random as rand
+# START 3rd PARTY PACKAGE
 import moment
 # END
 
 ##################RESET CONTROLLER#####################
 logger = logging.getLogger("__name__")
-
-users = [create("admin", "admin", "Thats Mr Administrator to you."),
+# Did not include admin in users list due to pre-existing admin functionality with DJango
+users = [
 			create("john", "John", "John Smith"),
 			create("paul", "Paul", "Paul Farrington"),
 			create("chrisc", "Chris", "Chris Campbell"),
@@ -61,8 +63,6 @@ def processReset(request):
     blabsStatement = None
     commentsStatement = None
     now = moment.now().format("YYYY-MM-DD")
-'''
-    rand = random.random()
 
     # Drop existing tables and recreate from schema file
     #recreateDatabaseSchema()
@@ -70,9 +70,8 @@ def processReset(request):
     try:
         logger.info("Getting Database connection")
         # Get the Database Connection
-        with transaction.atomic():
-            with connection.cursor() as cursor:
-
+        with connection.cursor() as cursor:
+            with transaction.atomic():
                 # Add the users
                 logger.info("Preparing the Stetement for adding users")
                 usersStatement = "INSERT INTO users (username, password, password_hint, created_at, last_login, real_name, blab_name) values ('%s', '%s', '%s', '%s', '%s', '%s', '%s');"
@@ -81,95 +80,79 @@ def processReset(request):
                     cursor.execute(usersStatement % (user.username, user.password, user.password_hint,
                                                      user.last_login, user.real_name, user.blab_name))
 
-        # Add the listeners
-        logger.info("Preparing the Stetement for adding listeners")
-        listenersStatement = "INSERT INTO listeners (blabber, listener, status) values ('%s', '%s', 'Active');"
-        for (int i = 1; i < users.length; i++) {
-            for (int j = 1; j < users.length; j++) {
-                if (rand.nextBoolean() && i != j) {
-                    String blabber = user.getUserName();
-                    String listener = users[j].getUserName();
+            # Add the listeners
+            logger.info("Preparing the Statement for adding listeners")
+            with transaction.atomic():
+                listenersStatement = "INSERT INTO listeners (blabber, listener, status) values ('%s', '%s', 'Active');"
+                for blabber in users:
+                    for listener in users:
+                        if rand.choice([False, True]) and (blabber != listener):
+                            
 
-                    logger.info("Adding " + listener + " as a listener of " + blabber);
+                            logger.info("Adding " + listener.username + " as a listener of " + blabber.username)
 
-                    listenersStatement.setString(1, blabber);
-                    listenersStatement.setString(2, listener);
+                            cursor.execute(listenersStatement % (blabber.username, listener.username))    
 
-                    listenersStatement.executeUpdate();
-                }
-            }
-        }
-        connect.commit();
+            # Fetch pre-loaded Blabs
+            logger.info("Reading blabs from file")
+            with transaction.atomic():
+                blabsContent = loadFile("blabs.txt")
 
-        // Fetch pre-loaded Blabs
-        logger.info("Reading blabs from file");
-        String[] blabsContent = loadFile("blabs.txt");
+                # Add the blabs
+                logger.info("Preparing the Statement for adding blabs")
+                blabsStatement = "INSERT INTO blabs (blabber, content, timestamp) values ('%s', '%s', '%s');"
+                for blabContent in blabsContent:
+                    # Get the array offset for a random user
+                    randomUserOffset = rand.randint(len(users) - 1)
 
-        // Add the blabs
-        logger.info("Preparing the Statement for adding blabs");
-        blabsStatement = connect
-                .prepareStatement("INSERT INTO blabs (blabber, content, timestamp) values ('%s', '%s', '%s');");
-        for (String blabContent : blabsContent) {
-            // Get the array offset for a random user, except admin who's offset 0.
-            int randomUserOffset = rand.nextInt(users.length - 2) + 1;
+                    # get the number or seconds until some time in the last 30 days.
+                    vary = rand.randint(0,(30 * 24 * 3600)+1)
 
-            // get the number or seconds until some time in the last 30 days.
-            long vary = rand.nextInt(30 * 24 * 3600);
+                    username = users[randomUserOffset].username
+                    logger.info("Adding a blab for " + username)
+                    cursor.execute(blabsStatement % (username, blabContent, moment.now().sub(seconds=(vary*1000)).format("YYYY-MM-DD hh:mm:ss")))
 
-            String username = users[randomUserOffset].getUserName();
-            logger.info("Adding a blab for " + username);
+            # Fetch pre-loaded Comments
+            logger.info("Reading comments from file")
+            commentsContent = loadFile("comments.txt")
 
-            blabsStatement.setString(1, username);
-            blabsStatement.setString(2, blabContent);
-            blabsStatement.setTimestamp(3, new Timestamp(now.getTime() - (vary * 1000)));
+            # Add the comments
+            with transaction.atomic():
+                logger.info("Preparing the Statement for adding comments")
+                commentsStatement = "INSERT INTO comments (blabid, blabber, content, timestamp) values ('%s', '%s', '%s', '%s');"
+                for i in range(len(blabsContent)):
+                    # Add a random number of comment
+                    count = rand.randint(0,5) # between 0 and 6
 
-            blabsStatement.executeUpdate();
-        }
-        connect.commit();
+                    for j in range(len(count)) :
+                        # Get the array offset for a random user
+                        randomUserOffset = rand.randint(0,users.length-1) #removed +1 cause no admin,  removed -2 because no admin and inclusive.
+                        username = users[randomUserOffset].username
 
-        // Fetch pre-loaded Comments
-        logger.info("Reading comments from file");
-        String[] commentsContent = loadFile("comments.txt");
+                        # Pick a random comment to add
+                        commentNum = rand.randint(0,len(commentsContent)-1)
+                        comment = commentsContent[commentNum]
 
-        // Add the comments
-        logger.info("Preparing the Statement for adding comments");
-        commentsStatement = connect.prepareStatement(
-                "INSERT INTO comments (blabid, blabber, content, timestamp) values ('%s', '%s', '%s', '%s');");
-        for (int i = 1; i <= blabsContent.length; i++) {
-            // Add a random number of comment
-            int count = rand.nextInt(6); // (between 0 and 6)
+                        # get the number or seconds until some time in the last 30 days.
+                        vary = rand.randint(0,(30 * 24 * 3600)+1)
 
-            for (int j = 0; j < count; j++) {
-                // Get the array offset for a random user, except admin who's offset 0.
-                int randomUserOffset = rand.nextInt(users.length - 2) + 1;
-                String username = users[randomUserOffset].getUserName();
+                        logger.info("Adding a comment from " + username + " on blab ID " + str(i))
 
-                // Pick a random comment to add
-                int commentNum = rand.nextInt(commentsContent.length);
-                String comment = commentsContent[commentNum];
+                        cursor.execute(commentsStatement % (i,username,comment,moment.now().sub(seconds=(vary*1000)).format("YYYY-MM-DD hh:mm:ss")))      
+    except IntegrityError as er:
+         logger.error(er)
+    except sqlite3.Error as ex :
+        logger.error(ex.sqlite_errorcode, ex.sqlite_errorname)
 
-                // get the number or seconds until some time in the last 30 days.
-                long vary = rand.nextInt(30 * 24 * 3600);
-
-                logger.info("Adding a comment from " + username + " on blab ID " + String.valueOf(i));
-                commentsStatement.setInt(1, i);
-                commentsStatement.setString(2, username);
-                commentsStatement.setString(3, comment);
-                commentsStatement.setTimestamp(4, new Timestamp(now.getTime() - (vary * 1000)));
-
-                commentsStatement.executeUpdate();
-            }
-        }
-        connect.commit();
-    except SQLException | ClassNotFoundException ex :
-        logger.error(ex);
-    return redirect("reset");
+    return redirect("reset")
 
 
 
 #Drop and recreate the entire database schema
 
 def recreateDatabaseSchema():
+    pass
+'''
         # Fetch database schema
         logger.info("Reading database schema from file")
         String[] schemaSql = loadFile("blab_schema.sql", new String[] { "--", "/*" }, ";");
@@ -212,9 +195,52 @@ def recreateDatabaseSchema():
         }
     }
 '''
-def create(userName, blabName,realName):
-    password = userName
-    dateCreated = moment.now().format("YYYY-MM-DD hh:mm:ss")
-    lastLogin = None
 
-    return User(userName, password, dateCreated, lastLogin, blabName, realName)
+def loadFile(filename, skipCharacters='', delimiter='/'):
+    pass
+'''
+		path = "/app/src/main/resources" + File.separator + filename;
+
+		regex = ""
+		if len(skipCharacters) > 0 :
+			skipString = String.join("|", skipCharacters);
+			skipString = skipString.replaceAll("(?=[]\\[+&!(){}^\"~*?:\\\\])", "\\\\");
+			regex = "^(" + skipString + ").*?";
+		}
+
+		String[] lines = null;
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(path));
+
+			String line = br.readLine();
+			while (line != null) {
+				if (line.matches(regex)) {
+					line = br.readLine();
+					continue;
+				}
+
+				sb.append(line);
+				sb.append(System.lineSeparator());
+
+				line = br.readLine();
+			}
+
+			// Break content by delimiter
+			lines = sb.toString().split(delimiter);
+		} catch (IOException ex) {
+			logger.error(ex);
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+			} catch (IOException ex) {
+				logger.error(ex);
+			}
+		}
+
+		return lines;
+	}
+    '''
